2910bc834b469111c8626a3d613322bccea487bd
YJS96 <wnt9864@hotmail.com>
2024-11-19 20:05:50 +0900

    :sparkles: feat: 교육 영상, 카드뉴스, 퀴즈 구현

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,6 +5,9 @@ import MissionMain from '@/pages/mission/MissionMain';
 import EduMain from '@/pages/edu/EduMain';
 import MyPage from '@/pages/mypage/MyPage';
 
+import EduQuiz from '@/pages/edu/EduQuiz';
+import QuizResult from '@/pages/edu/QuizResult';
+
 export default function Router() {
   return (
     <BrowserRouter>
@@ -12,6 +15,8 @@ export default function Router() {
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/mission" element={<MissionMain />}></Route>
         <Route path="/edu" element={<EduMain />}></Route>
+        <Route path="/edu/:id" element={<EduQuiz />}></Route>
+        <Route path="/edu/result" element={<QuizResult />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
       </Routes>
     </BrowserRouter>
