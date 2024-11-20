f8f0d72dd8e734425c96ec035c3f7ffaeaaf074e
Doyeon <duseh729@naver.com>
2024-11-21 06:49:45 +0900

    feat: mission 조회 및 디스플레잉 완료 미션 현재 D-DAY가 남아있는데 알아서 수정 부탁!

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,6 @@
 import axiosInstance from "./instance"
 
 export const login = async(id: string, password: string)=>{
-    const response = await axiosInstance.post(`/api/auth/login`, {id: id, password: password});
+    const response = await axiosInstance.post(`/auth/login`, {id: id, password: password});
     return response;
 }
